4c645efe393a97b794efaebcca8c1fbfce402d88
jeremytubbs <jeremytubbs@gmail.com>
2016-12-21 01:12:42 -0500

    add urls to serializers

--- person/serializers.py
+++ person/serializers.py
@@ -6,28 +6,28 @@ class AddressSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Address
-        fields = ('street', 'city', 'state', 'postal_code', 'address_type')
+        fields = ('street', 'city', 'state', 'postal_code', 'address_type', 'url')
 
 
 class EmailAddressSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = EmailAddress
-        fields = ('email', 'email_type')
+        fields = ('email', 'email_type', 'url')
 
 
 class PhoneSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Phone
-        fields = ('phone', 'phone_type')
+        fields = ('phone', 'phone_type', 'url')
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
     addresses = AddressSerializer(read_only=True, many=True)
     emails = EmailAddressSerializer(read_only=True, many=True)
     phones = PhoneSerializer(read_only=True, many=True)
 
     class Meta:
         model = Name
-        fields = ('first_name', 'last_name', 'note', 'phones', 'addresses', 'emails')
+        fields = ('first_name', 'last_name', 'note', 'phones', 'addresses', 'emails', 'url')
