b6f55013dc2000c37feff65c81dc2b87374f2803
Jeremy Tubbs <jtubbs@Jeremys-MacBook-Pro.local>
2016-12-20 23:30:52 -0500

    use extra not max_num for admin

--- person/admin.py
+++ person/admin.py
@@ -3,17 +3,17 @@ from person.models import Name, Address, EmailAddress, Phone
 
 class AddressInline(admin.TabularInline):
     model = Address
-    max_num = 1
+    extra = 1
 
 
 class EmailInline(admin.TabularInline):
     model = EmailAddress
-    max_num = 1
+    extra = 1
 
 
 class PhoneInline(admin.TabularInline):
     model = Phone
-    max_num = 1
+    extra = 1
 
 
 class PersonAdmin(admin.ModelAdmin):
