719e9ebf2dc64ddb8a29dc0174555fa9d338f0b4
jeremytubbs <jeremytubbs@gmail.com>
2016-12-21 13:05:54 -0500

    basic setup for angular

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', ensure_csrf_cookie(TemplateView.as_view(template_name="home.html"))),
     url(r'^contacts/', include('person.urls')),
 ]
